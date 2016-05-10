73f5c6718c5a7f9527deb624e8130d6f0da72563
Patrick H. Lauke <redux@splintered.co.uk>
2016-05-10 20:50:44 +0100

    Change local time to 12 hour am/pm clock

--- id24.js
+++ id24.js
@@ -41,7 +41,8 @@ function pad(num, size) {
 
     $('.session').each(function () {
         var local = new Date(parseInt($(this).attr('data-time-gmt')));
-        $(this).find('span.session_time_local').text(local.getHours() + ':' + pad(local.getMinutes(), 2));
+        $(this).find('span.session_time_local').text((local.getHours() == 0 ? "12" : local.getHours() > 12 ? local.getHours() - 12 : local.getHours()) + ':' + pad(local.getMinutes(), 2));
+        $(this).find('span.session_time_local_ampm').text(local.getHours() < 12 ? "AM" : "PM");
         $(this).find('span.session_time_utc').text(local.getUTCHours() + ':' + pad(local.getUTCMinutes(), 2));
         if (local.getUTCDate() !== local.getDate()) {
             $(this).find('span.session_day_local').text(local.getDate() + ' ' + months[local.getMonth()]).removeClass('hidden');
