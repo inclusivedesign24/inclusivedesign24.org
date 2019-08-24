4d88d04955a178477c1fc8c9defebc09d4bf03a2
Ian Pouncey <ipouncey@Ians-MacBook-Pro-269.local>
2019-08-25 00:11:16 +0100

    Adds Eleventy and generated 2019 schedule

---
+++ .eleventy.js
@@ -0,0 +1,13 @@
+module.exports = function(eleventyConfig) {
+    eleventyConfig.addPassthroughCopy("_redirects");
+
+    return {
+        templateFormats: [
+            "png",
+            "svg",
+            "css",
+            "js",
+            "html"
+        ]
+    };
+}
