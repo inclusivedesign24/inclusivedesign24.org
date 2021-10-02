de2f59362153023c0bba9d9cce9868bd2e334b26
Patrick H. Lauke <redux@splintered.co.uk>
2021-10-02 13:57:34 +0100

    Update 11ty

--- .eleventy.js
+++ .eleventy.js
@@ -1,17 +1,18 @@
 module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("_redirects");
     eleventyConfig.addPassthroughCopy("site.webmanifest");
+    eleventyConfig.addPassthroughCopy("**/*.js");
+    eleventyConfig.addPassthroughCopy("**/*.css");
+    eleventyConfig.addPassthroughCopy("**/*.zip");
+    eleventyConfig.addPassthroughCopy("**/*.pdf");
+    eleventyConfig.addPassthroughCopy("**/*.jpg");
+    eleventyConfig.addPassthroughCopy("**/*.png");
+    eleventyConfig.addPassthroughCopy("**/*.ico");
+    eleventyConfig.addPassthroughCopy("**/*.svg");
 
     return {
         templateFormats: [
-            "png",
-            "svg",
-            "jpg",
-            "css",
-            "js",
-            "html",
-            "zip",
-            "ico"
+            "html"
         ]
     };
 }
