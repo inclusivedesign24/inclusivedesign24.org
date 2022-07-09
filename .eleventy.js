b4134464e89095cffdcdc60aa7d7e46ce4a90dc6
Patrick H. Lauke <redux@splintered.co.uk>
2022-07-09 10:48:06 +0100

    Reorganise eleventy folder structure
    * old script naively copied everything (include node_modules etc) into the `_site` output folder
    * this now uses same approach as tetralogical sites, cleanly splits `src` and `dist` output
    * allows for clean local development as well

--- .eleventy.js
+++ .eleventy.js
@@ -1,18 +1,27 @@
-module.exports = function(eleventyConfig) {
-    eleventyConfig.addPassthroughCopy("_redirects");
-    eleventyConfig.addPassthroughCopy("site.webmanifest");
-    eleventyConfig.addPassthroughCopy("**/*.js");
-    eleventyConfig.addPassthroughCopy("**/*.css");
-    eleventyConfig.addPassthroughCopy("**/*.zip");
-    eleventyConfig.addPassthroughCopy("**/*.pdf");
-    eleventyConfig.addPassthroughCopy("**/*.jpg");
-    eleventyConfig.addPassthroughCopy("**/*.png");
-    eleventyConfig.addPassthroughCopy("**/*.ico");
-    eleventyConfig.addPassthroughCopy("**/*.svg");
+const markdownIt = require("markdown-it");
+
+module.exports = config => {
+    config.addPassthroughCopy('./src/_redirects');
+    config.addPassthroughCopy('./src/robots.txt');
+    config.addPassthroughCopy('./src/site.webmanifest');
+    config.setTemplateFormats("html,css,js,liquid,ejs,md,hbs,mustache,haml,pug,njk,jpg,jpeg,gif,png,svg,webp,ico,pdf,ppt,pptx,doc,docx,zip");
+
+    // Ad-hoc markdown filter
+    const md = new markdownIt({
+        html: true
+    });
+    config.addFilter("markdown", (content) => {
+        return md.render(content);
+    });
 
     return {
         templateFormats: [
             "html"
-        ]
+        ],
+        dir: {
+            input: 'src',
+            output: 'dist',
+            includes: "_includes"
+        }
     };
 }
